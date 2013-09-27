201264012f0113ac72141de87f440e18b69a8e97
Abizer Nasir <abizern@abizern.org>
2013-09-27 19:15:33 +0100

    Add scenes and test flip transition

---
+++ BeaChord/BeaChord/BCConfigurationViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCConfigurationViewController.h
+//  BeaChord
+//
+//  Created by Abizer Nasir on 27/09/2013.
+//  Copyright (c) 2013 NSCoderNight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCConfigurationViewController : UIViewController
+
+@end
---
+++ BeaChord/BeaChord/BCConfigurationViewController.m
@@ -0,0 +1,18 @@
+//
+//  BCConfigurationViewController.m
+//  BeaChord
+//
+//  Created by Abizer Nasir on 27/09/2013.
+//  Copyright (c) 2013 NSCoderNight. All rights reserved.
+//
+
+#import "BCConfigurationViewController.h"
+
+@interface BCConfigurationViewController ()
+
+@end
+
+@implementation BCConfigurationViewController
+
+
+@end
