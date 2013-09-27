1879aa04aad93c5e1af29d52aa3f05aa604223ed
Francesco Frison <cescofry@gmail.com>
2013-09-27 14:52:21 +0100

    Added chord class

---
+++ BeaChord/BeaChord/Models/BCChord.h
@@ -0,0 +1,13 @@
+//
+//  BCChord.h
+//  BeaChord
+//
+//  Created by Francesco Frison on 27/09/2013.
+//  Copyright (c) 2013 NSCoderNight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCChord : NSObject
+
+@end
---
+++ BeaChord/BeaChord/Models/BCChord.m
@@ -0,0 +1,13 @@
+//
+//  BCChord.m
+//  BeaChord
+//
+//  Created by Francesco Frison on 27/09/2013.
+//  Copyright (c) 2013 NSCoderNight. All rights reserved.
+//
+
+#import "BCChord.h"
+
+@implementation BCChord
+
+@end
