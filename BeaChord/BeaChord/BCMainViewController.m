20178b5720823d0ade70461c844a803f5178923d
Francesco Frison <cescofry@gmail.com>
2013-09-27 21:22:40 +0100

    actions on main controller

--- BeaChord/BeaChord/BCMainViewController.m
+++ BeaChord/BeaChord/BCMainViewController.m
@@ -15,7 +15,9 @@
 @property (nonatomic, strong) IBOutlet UISegmentedControl *segmentedControl;
 @property (nonatomic, strong) IBOutlet UIButton *startButton;
 
-
+- (IBAction)switchModeAction:(id)sender;
+- (IBAction)changedSegment:(id)sender;
+- (IBAction)startButtonAction:(id)sender;
 
 @end
 
@@ -27,6 +29,21 @@
     
     
     
+}
+
+
+- (IBAction)switchModeAction:(id)sender {
+    BOOL isBroadcast = [(UISwitch *)sender isOn];
+    [self.segmentedControl setAlpha:(isBroadcast)? 1.0 : 0.0];
+}
+
+- (IBAction)changedSegment:(id)sender {
+    NSInteger segment = [(UISegmentedControl *)sender selectedSegmentIndex];
+    NSLog(@"Selected index %d", segment);
+}
+
+- (IBAction)startButtonAction:(id)sender {
+    NSLog(@"start");
 }
 
 @end
