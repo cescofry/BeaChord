001b925cc11d11dc3bf5c077040dfa26ae642e65
Francesco Frison <cescofry@gmail.com>
2013-09-27 11:44:40 +0100

    init

---
+++ BeaChord/BeaChord/BCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCAppDelegate.h
+//  BeaChord
+//
+//  Created by Francesco Frison on 27/09/2013.
+//  Copyright (c) 2013 NSCoderNight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeaChord/BeaChord/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BeaChord
+//
+//  Created by Francesco Frison on 27/09/2013.
+//  Copyright (c) 2013 NSCoderNight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BCAppDelegate class]));
+    }
+}
