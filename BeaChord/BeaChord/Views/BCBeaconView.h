1c018d03c6f64affb9471e3650cf3341000f57b6
Francesco Frison <cescofry@gmail.com>
2013-09-27 21:43:51 +0100

    Add beacon view

---
+++ BeaChord/BeaChord/Views/BCBeaconView.h
@@ -0,0 +1,13 @@
+//
+//  BCBeaconView.h
+//  BeaChord
+//
+//  Created by Francesco Frison on 27/09/2013.
+//  Copyright (c) 2013 NSCoderNight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCBeaconView : UIView
+
+@end
